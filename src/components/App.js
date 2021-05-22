8e670b228418cb9264fdd35b4c0ace2f34d213ac
Cao Minh <trancaominh1997@gmail.com>
2021-05-22 10:32:35 +0700

    code upload

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import '../App.css';
 import NavBarHeader from "./navBarHeader";
 import BannerHeader from "./bannerHeader";
 import HomeTools from "./homeTools";
-
+import FilmBlock from "./filmBlock";
 function App() {
   return (
     <div className="App">
         <NavBarHeader/>
         <BannerHeader/>
         <HomeTools/>
+        <FilmBlock/>
     </div>
   );
 }
---
+++ src/components/filmBlock.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+class FilmBlock extends Component {
+    render() {
+        return (
+            <section className="filmBlock">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/04/trang-ti-16194120693380_215x318.jpg")'}}>
+                            </div>
+                            <div className="row">
+                                alo
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/04/lat-mat-48h-16176188609123_215x318.png")'}}>
+
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/03/thien-than-ho-menh-16157907251398_215x318.jpg")'}}>
+
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/04/detective-conan-scarlet-bullet-tham-tu-lung-danh-conan-vien-dan-do-p-16185623740090_215x318.png")'}}>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row ">
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/03/nguoi-nhan-ban-seobok-c18-16170933054901_215x318.jpg")'}}>
+
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/03/ban-tay-diet-quy-evil-expeller-c16-16167437811994_215x318.png")'}}>
+
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/04/ke-vo-danh-nobody-c18-16177813592209_215x318.png")'}}>
+
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <div className="posterFilm" style={{backgroundImage: 'url("https://s3img.vcdn.vn/mobile/123phim/2021/04/detective-conan-scarlet-bullet-tham-tu-lung-danh-conan-vien-dan-do-p-16185623740090_215x318.png")'}}>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default FilmBlock;
